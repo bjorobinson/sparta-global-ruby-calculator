00af56ba673370e99e54bd8ec50bd34ae2336f04
Tech-A12 <tech-a12@Tech-A12s-Air.home>
2018-02-12 19:20:33 +0000

    Added planned structure of app file

---
+++ app.rb
@@ -0,0 +1,15 @@
+# Functions
+  # Input
+  # Basic
+    # addition
+    # subtraction
+    # multiplication
+    # division
+  # Advanced
+    # power
+    # square root
+  # BMI Calc
+  # Journey Calc
+  # Output
+
+# Main
