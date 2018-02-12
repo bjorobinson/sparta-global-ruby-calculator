979543c374ab9db9bd8dd0c56f63050031ae78e9
Tech-A12 <tech-a12@Tech-A12s-Air.home>
2018-02-12 19:37:56 +0000

    Added operator interpretation and addition

--- app.rb
+++ app.rb
@@ -1,11 +1,22 @@
 # Functions
   # Input
   def getInput
-    puts 'Enter Input:'
-    return gets.chomp
+    puts 'Enter Input in the form x operator y, eg. 4 + 9:'
+    gets.chomp
+  end
+  def whatInput input
+    input_array = input.split
+    operator = input_array[1]
+    case operator
+    when '+'
+      putsOutput add input_array[0].to_f,input_array[2].to_f
+    end
   end
   # Basic
     # addition
+    def add num1,num2
+      num1+num2
+    end
     # subtraction
     # multiplication
     # division
@@ -17,9 +28,9 @@
   # Output
   def putsOutput output
     puts 'Answer:'
-    puts output.to_str
+    puts output
   end
 
 # Main
 test_in = getInput
-putsOutput test_in
+whatInput(test_in)
